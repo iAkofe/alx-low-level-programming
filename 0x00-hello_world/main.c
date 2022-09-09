34a9097d116cb99d51f50d0849ba0dab6605e876
iAkofe <toluwalopeolasupo@gmail.com>
2022-09-08 19:14:07 -0700

    task 0 again

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
 {
-		printf("Holberton School");
-			return (0);
+	return (0);
 }
