830161d9a88126b889a9d23e92e6c355d7d4bd80
Toluwalope Olasupo <toluwalopeolasupo@gmail.com>
2022-12-11 11:42:55 +0100

    update task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,38 +1,14 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts node at given pos
- * @h: pointer to head of the dll
- * @idx: index position to add node
- * @n: integer data for the new node
- * Return: returns address of the new pointer or NULL
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to head of list
+ * @idx: index of the list where the new node should be added
+ * @n: data of the new node
+ * 
+ * Return:  the address of the new node, or NULL if it failed
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *last_node = *h;
-	unsigned int count = 0;
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	while (last_node->next != NULL)
-	{
-		if (count == idx)
-		{
-			new_node->n = n;
-			new_node->next = last_node;
-			new_node->prev = last_node->prev;
-			last_node->prev->next = new_node;
-			last_node->prev = new_node;
-			return (new_node);
-		}
-		last_node = last_node->next;
-		count++;
-	}
-	if (count++ == idx)
-		return (add_dnodeint_end(h, n));
-	return (NULL);
 }
